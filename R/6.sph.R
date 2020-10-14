d30b61a206fb512c46cd73946984be1f298c3d73
Martin Bladt <martinbladt@gmail.com>
2020-10-15 00:01:28 +0200

    Constructor for susrvival analysis

--- R/6.sph.R
+++ R/6.sph.R
@@ -16,3 +16,29 @@ setClass("sph",
          )
 )
 
+#' Constructor Function for Survival phase type objects
+#'
+#' @param ph An object of class \linkS4class{ph}
+#' @param iph An object of class \linkS4class{iph}
+#'
+#' @return An object of class \linkS4class{sph}.
+#' @export
+#'
+#' @examples
+sph <- function(ph = NULL, iph = NULL, coefs = list(B = NULL, C = NULL)) {
+  if (is.null(iph)) {
+    if (is.null(ph)) {
+      iph <- ph(structure = "General")
+    }else{iph <- ph}
+    gfun <- list(name = "Indentity", pars = numeric(0))
+  }else{
+    gfun <- iph@gfun
+  }
+  iph@pars
+  new("sph",
+      name = paste("survival ", iph@name, sep = ""),
+      pars = iph@pars,
+      gfun = gfun,
+      coefs = coefs
+  )
+}
