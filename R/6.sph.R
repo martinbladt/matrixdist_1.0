6f2471b6238a6ad21412602d3c6fbf91200c93af
Martin Bladt <martinbladt@gmail.com>
2020-10-14 23:30:24 +0200

    Survival def.

---
+++ R/6.sph.R
@@ -0,0 +1,17 @@
+#' Survival Analysis for Phase Type distributions
+#'
+#' Class of objects for inhomogeneous phase type distributions
+#'
+#' @slot name name of the phase type distribution
+#' @slot pars a list comprising of the parameters.
+#'
+#' @return
+#' @export
+#'
+setClass("sph",
+         contains = c("ph", "iph"),
+         slots = list(
+           gfun = "list"
+         )
+)
+
